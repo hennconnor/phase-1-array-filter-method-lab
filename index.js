af1b2c6d58c4867bff3bcdb10b43815bebab58f4
Connor Henn <connor.henn@yahoo.com>
2022-07-24 18:32:03 -0700

    commit tree b81e5d44d987e988642852565b5147fb7935477c

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solution here
+function findMatching(drivers, string){
+    const newArray = [];
+    drivers.filter(name => {
+    if (name.toLowerCase()  === string.toLowerCase()){
+        newArray.push(name);
+    }
+});
+return newArray;
+}
+
+function fuzzyMatch(drivers, string){
+    const newArray = [];
+    drivers.filter(name => {
+        if (name[0] === string[0]){
+            newArray.push(name);
+        }
+    });
+    return newArray;
+}
+
+function matchName(drivers, string){
+    const newArray = [];
+    drivers.filter(driver => {
+        if(driver.name === string){
+            newArray.push(driver);
+        }
+    });
+    return newArray;
+}
+
